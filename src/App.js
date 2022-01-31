d372b918945f5b5978414871a0eeee48fc908ef6
unknown <clamy6@yahoo.fr>
2022-01-31 13:56:38 +0100

    new form

--- src/App.js
+++ src/App.js
@@ -4,45 +4,47 @@ import "bootstrap/dist/css/bootstrap.min.css";
 class App extends React.Component {
   render() {
     return (
-      <div>
-        <form>
-          <div className="form-group">
-            <label>Email</label>
-            <input type="email" className="form-control" />
+      <div className="container">
+        <div className="row d-flex justify-content-center">
+          <div className="col col-lg-6 ">
+            <h1 className="text-center">Login</h1>
+            <form class="">
+              <label for="email" className="form-label mt-2">
+                Email address
+              </label>
+              <input
+                type="email"
+                className="form-control"
+                id="email"
+                placeholder="Enter email..."
+                required
+              />
+              <label for="password" className="form-label mt-2">
+                Password
+              </label>
+              <input
+                type="password"
+                className="form-control"
+                id="password"
+                placeholder="Enter password..."
+                required
+              />
+              <div className="mt-2 form-check">
+                <input
+                  type="checkBox"
+                  className="form-check-input"
+                  id="check"
+                />
+                <label className="form-check-label" for="checkBox">
+                  Remember me
+                </label>
+              </div>
+              <button type="submit" className="btn btn-primary mt-2">
+                Submit
+              </button>
+            </form>
           </div>
-
-          <div className="form-group">
-            <label>Password</label>
-            <input type="text" className="form-control" />
-          </div>
-
-          <button type="submit" className="btn btn-block btn-primary">
-            Submit
-          </button>
-        </form>
-        {/* <form>
-          <div className="mb-3">
-            <label for="exampleFormControlInput1" className="form-label">
-              Email address
-            </label>
-            <input
-              type="email"
-              className="form-control"
-              id="exampleFormControlInput1"
-              placeholder="name@example.com"
-            />
-          </div>
-          <div className="mb-3">
-            <label for="exampleFormControlTextarea1" className="form-label">
-              Example textarea
-            </label>
-            <textarea
-              className="form-control"
-              id="exampleFormControlTextarea1"
-              rows="3"
-            ></textarea>
-          </div>
-        </form> */}
+        </div>
       </div>
     );
   }
