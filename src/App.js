220648c69cc6f3399c4bfa6a23b1d2d845008bc8
unknown <clamy6@yahoo.fr>
2022-01-31 12:23:36 +0100

    add form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <form>
+          <div className="form-group">
+            <label>Email</label>
+            <input type="email" className="form-control" />
+          </div>
+
+          <div className="form-group">
+            <label>Password</label>
+            <input type="text" className="form-control" />
+          </div>
+
+          <button type="submit" className="btn btn-block btn-primary">
+            Submit
+          </button>
+        </form>
+        {/* <form>
+          <div className="mb-3">
+            <label for="exampleFormControlInput1" className="form-label">
+              Email address
+            </label>
+            <input
+              type="email"
+              className="form-control"
+              id="exampleFormControlInput1"
+              placeholder="name@example.com"
+            />
+          </div>
+          <div className="mb-3">
+            <label for="exampleFormControlTextarea1" className="form-label">
+              Example textarea
+            </label>
+            <textarea
+              className="form-control"
+              id="exampleFormControlTextarea1"
+              rows="3"
+            ></textarea>
+          </div>
+        </form> */}
+      </div>
+    );
+  }
 }
 
 export default App;
